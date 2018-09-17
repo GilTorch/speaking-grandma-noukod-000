c43de02573691476f0a666b057da5a4ac1fe78fd
Gilbert Torchon <gilbert@noukod.com>
2018-09-17 16:18:26 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,6 +4,8 @@ def speak_to_grandma(phrase)
       return "I LOVE YOU TOO PUMPKIN!"
    elsif phrase == "Hi Nana, how are you?" || phrase == "Hi!"
       return "HUH?! SPEAK UP, SONNY!"
+   elsif phrase == "WHAT DID YOU EAT TODAY?"
+      return "NO, NOT SINCE 1938!"
    end
 end
 # Whatever you say to grandma, she should respond with
