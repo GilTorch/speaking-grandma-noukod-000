5bb986ad7d57421afa70e7230a33e1522f45128d
Gilbert Torchon <gilbert@noukod.com>
2018-09-17 16:18:48 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
       return "I LOVE YOU TOO PUMPKIN!"
    elsif phrase == "Hi Nana, how are you?" || phrase == "Hi!"
       return "HUH?! SPEAK UP, SONNY!"
-   elsif phrase == "WHAT DID YOU EAT TODAY?"
+   elsif phrase == "WHAT DID YOU EAT TODAY?" || phrase == "WHAT?"
       return "NO, NOT SINCE 1938!"
    end
 end
